96c2c7bce13a00cfac92bf521dde4e9db168a350
PROSPER <linautatum501@gmail.com>
2024-05-11 16:41:48 +0300

    <Updt 50%>

--- IOT_Controller/IOT_Controller/obj/Debug/net8.0-android/IOT Controller.AssemblyInfo.cs
+++ IOT_Controller/IOT_Controller/obj/Debug/net8.0-android/IOT Controller.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IOT Controller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d1997537d2762f1d6399959a391ef921d6d3973b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0537bbfc5caf9b58efef701d12ceea3dd6f5a688")]
 [assembly: System.Reflection.AssemblyProductAttribute("IOT Controller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IOT Controller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
