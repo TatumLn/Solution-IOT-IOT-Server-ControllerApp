f4563813a89becf357a1fb0da4e925a0f358a808
PROSPER <linautatum501@gmail.com>
2024-06-21 12:04:30 +0300

    updt<version 0.7>

--- IOT_Controller/IOT_Controller/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/IOT Controller.AssemblyInfo.cs
+++ IOT_Controller/IOT_Controller/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/IOT Controller.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IOT Controller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+dc5c317e0da49725cfb8b866ff79df4a1b6ce302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f4f6e016103068fb51664c92ed022ad833876422")]
 [assembly: System.Reflection.AssemblyProductAttribute("IOT Controller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IOT Controller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
